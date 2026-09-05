7a43c3371f77eda79f2bd9f48d5bd7b4ca006cf5
Sabari293 <sabarinath7607@gmail.com>
2026-09-05 19:27:24 +0530

    updated

--- task1/src/conv_tile.cpp
+++ task1/src/conv_tile.cpp
@@ -7,19 +7,23 @@ void conv_tile(const float* in, float* out, const float* ker,
     // TODO(student): replace this placeholder with your tiled/blocked implementation.
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
-    for(int oy=0;oy<H;oy++){
-        for(int ox=0;ox<W;ox++){
-            out[oy*W+ox]=0.0f;
-        }
-    }
-    for (int ky = 0; ky < K; ++ky) { 
-        for (int kx = 0; kx < K; ++kx) {
-            float ker_val=ker[ky*K+kx];
-            for (int oy = 0; oy < H; ++oy) {           
-                for (int ox = 0; ox < W; ++ox) {
-                    out[oy*W+ox] += in[(oy + ky) * in_stride + (ox + kx)] * ker_val;
+    int T=144;
+    for(int oys = 0;oys<H ;oys+=T){
+        for(int oxs = 0;oxs<W;oxs+=T){
+            int oye = oys+T>H?H:oys+T;
+            int oxe = oxs+T>W?W:oxs+T;
+            for (int oy = oys; oy < oye; ++oy) {    
+                for (int ox = oxs; ox < oxe; ++ox) {
+                    float acc = 0.0f;
+                    for (int ky = 0; ky < K; ++ky) {
+                        for (int kx = 0; kx < K; ++kx) {
+                            acc += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
+                        }
+                    }
+                    out[oy * W + ox] = acc;
                 }
             }
         }
     }
+    
 }
